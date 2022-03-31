fa67f40c81f1e69d22a43a0d8b9bbfe9bb5f821d
Luke Gruenwald <lgruenwald@sbcglobal.net>
2022-03-31 10:53:38 -0500

    add firebase sign up with google

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider} from 'firebase/auth'
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore'
 
 const firebaseConfig = {
     apiKey: "AIzaSyBaID39XaPeCSS5r159yhdLl1tTxsj9aCw",
@@ -17,9 +18,33 @@ const firebaseApp = initializeApp(firebaseConfig);
 export const auth = getAuth();
 
 const provider = new GoogleAuthProvider();
-
 provider.setCustomParameters({
     prompt: "select_account"
 });
 
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider)
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+
+    const userSnapshot = await getDoc(userDocRef);
+
+    if(!userSnapshot.exists()) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt
+            });
+        } catch(error) {
+            console.log('error creating user', error.message)
+        }
+    } 
+
+    return userDocRef;
+}
