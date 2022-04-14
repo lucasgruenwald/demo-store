48c8a32315d467c3c01b52b7115c37efc096c47c
lukegruenwald <lgruenwald@sbcglobal.net>
2022-04-14 13:10:57 -0500

    create product card

---
+++ src/components/product-card/product-card.component.jsx
@@ -0,0 +1,25 @@
+import './product-card.styles.scss'
+
+import Button from '../button/button.component';
+
+const ProductCard = ({ product }) => {
+ 
+    const { name, price, imageUrl} = product;
+
+    return(
+        <div>
+            <img src={imageUrl} alt={`${name}`} />
+            <div className='footer'>
+                <span className='name'>{name}</span>
+                <span className='price'>{price}</span>
+            </div>
+            <Button buttonType='inverted'>
+                Add to cart
+            </Button>
+        </div>
+    )
+
+}
+
+export default ProductCard;
+
