a154a522bfcb0ac7df16f9fb0a19dedbbcf35858
Luke Gruenwald <lgruenwald@sbcglobal.net>
2022-04-21 16:22:49 -0500

    update cart item count when cart items change

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 
 export const addCartItem = (cartItems, productToAdd) => {
     const existingCartItem = cartItems.find(
@@ -26,6 +26,7 @@ export const CartContext = createContext({
     setIsOpen: () => { },
     cartItems: [],
     addItemToCart: () => {},
+    cartItemCount: 0,
 });
 
 export const CartProvider = ({ children }) => {
@@ -33,9 +34,19 @@ export const CartProvider = ({ children }) => {
 
     const [cartItems, setCartItems] = useState([]);
 
+    const [cartItemCount, setCartItemCount] = useState(0);
+
+    useEffect(() => {
+        const count = cartItems.reduce(
+            (total, cartItem) => total + cartItem.quantity, 0
+        );
+        
+        setCartItemCount(count);
+    }, [cartItems]);
+
     const addItemToCart = (product) => setCartItems(addCartItem(cartItems, product));
 
-    const value = { isCartOpen, setIsCartOpen, cartItems, addItemToCart };
+    const value = { isCartOpen, setIsCartOpen, cartItems, addItemToCart, cartItemCount, };
 
     return(
         <CartContext.Provider value={value}>
