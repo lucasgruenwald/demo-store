74e4273fde7ea35ad9be17d8c5e84953d956ec2d
Luke Gruenwald <lgruenwald@sbcglobal.net>
2022-04-27 11:01:10 -0500

    add functionality to home directory

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,40 +1,46 @@
 import './directory.styles.scss'
 import DirectoryItem from '../directory-item/directory-item.component';
 
-const directoryArray = [
+  const categories = [
     {
-      id: 1,
-      title: 'Title 1',
-      imageUrl: 'https://ak.picdn.net/shutterstock/videos/19762045/thumb/1.jpg',
-    },
-    {
-      id: 2,
-      title: 'Title 2',
-      imageUrl: 'https://ak.picdn.net/shutterstock/videos/19762045/thumb/1.jpg',
-    },
-    {
-      id: 3,
-      title: 'Title 3',
-      imageUrl: 'https://ak.picdn.net/shutterstock/videos/19762045/thumb/1.jpg',
-    },
-    {
-      id: 4,
-      title: 'Title 4',
-      imageUrl: 'https://ak.picdn.net/shutterstock/videos/19762045/thumb/1.jpg',
-    },
-    {
-      id: 5,
-      title: 'Title 5',
-      imageUrl: 'https://ak.picdn.net/shutterstock/videos/19762045/thumb/1.jpg',
-    }
-  ]
-
-  const Directory = ({categories}) => {
+        id: 1,
+        title: 'Title 1',
+        imageUrl: 'https://ak.picdn.net/shutterstock/videos/19762045/thumb/1.jpg',
+        route: 'shop/hats'
+      },
+      {
+        id: 2,
+        title: 'Title 2',
+        imageUrl: 'https://ak.picdn.net/shutterstock/videos/19762045/thumb/1.jpg',
+        route: 'shop/jackets'
+      },
+      {
+        id: 3,
+        title: 'Title 3',
+        imageUrl: 'https://ak.picdn.net/shutterstock/videos/19762045/thumb/1.jpg',
+        route: 'shop/sneakers'
+      },
+      {
+        id: 4,
+        title: 'Title 4',
+        imageUrl: 'https://ak.picdn.net/shutterstock/videos/19762045/thumb/1.jpg',
+        route: 'shop/womens'
+      },
+      {
+        id: 5,
+        title: 'Title 5',
+        imageUrl: 'https://ak.picdn.net/shutterstock/videos/19762045/thumb/1.jpg',
+        route: 'shop/mens'
+      }
+   ]
+        
+  const Directory = () => {
+    
     return(
       <div className="directory-container">
-      {directoryArray.map((category) => (
-        <DirectoryItem key={category.id} category={category} />
-      ))}
+        {categories.map((category) => (
+          <DirectoryItem key={category.id} category={category} />
+        ))}
     </div>
     )
   }
--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -3,38 +3,11 @@ import { Outlet } from 'react-router-dom'
 import Directory from '../../components/directory/directory.component'
 
 const Home = () => {
-    const categories = [
-        {
-            id: 1,
-            title: 'Title 1',
-            imageUrl: 'https://ak.picdn.net/shutterstock/videos/19762045/thumb/1.jpg',
-          },
-          {
-            id: 2,
-            title: 'Title 2',
-            imageUrl: 'https://ak.picdn.net/shutterstock/videos/19762045/thumb/1.jpg',
-          },
-          {
-            id: 3,
-            title: 'Title 3',
-            imageUrl: 'https://ak.picdn.net/shutterstock/videos/19762045/thumb/1.jpg',
-          },
-          {
-            id: 4,
-            title: 'Title 4',
-            imageUrl: 'https://ak.picdn.net/shutterstock/videos/19762045/thumb/1.jpg',
-          },
-          {
-            id: 5,
-            title: 'Title 5',
-            imageUrl: 'https://ak.picdn.net/shutterstock/videos/19762045/thumb/1.jpg',
-          }
-        ]
 
         return (
           <div>
+            <Directory />
             <Outlet />
-            <Directory categories={categories}/>
           </div>
         )
 }
