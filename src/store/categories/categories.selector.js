7c17675773687af75befa8231b7c41c5086dd745
Luke Gruenwald <lgruenwald@sbcglobal.net>
2022-05-10 10:53:06 -0500

    memoize categories selector with reselect

--- src/store/categories/categories.selector.js
+++ src/store/categories/categories.selector.js
@@ -1,11 +1,19 @@
-export const selectCategoriesMap = (state) => {
-  console.log(state)
-  const categoriesMap = state.categories.categories.reduce(
+import { createSelector } from "reselect";
+
+const selectCategoryReducer = (state) => state.categories;
+
+// pure function, memoized, reruns only if input does not pass triple equality
+
+export const selectCategories = createSelector(
+  [selectCategoryReducer],
+  (categoriesSlice) => categoriesSlice.categories
+)
+
+export const selectCategoriesMap = createSelector(
+  [selectCategories], 
+  (categories) => categories.reduce(
     (acc, {title, items}) => {
       acc[title.toLowerCase()] = items;
       return acc
-    },
-    {}
-  );
-  return categoriesMap;
-}
+    },{})
+);
