454386dba3dbf2260ded67d22885c8b2221a9b2d
lucasgruenwald <lgruenwald@sbcglobal.net>
2022-08-07 16:06:39 -0500

    add sentry monitoring

--- src/index.js
+++ src/index.js
@@ -9,7 +9,20 @@ import App from './App';
 import { store, persistor } from './store/store';
 import { stripePromise } from './utils/stripe/stripe.utils'
 
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
 import './index.scss';
+
+Sentry.init({
+  dsn: "https://0efc8635e303465ebf813b6dfce3ee2d@o1349733.ingest.sentry.io/6629349",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 const rootElement = document.getElementById('root')
 
